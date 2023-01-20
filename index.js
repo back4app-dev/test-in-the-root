9190e3a93bdb81f30ba8c086bfaf00b174f15312
sadakchap <aliceprerna@gmail.com>
2023-01-20 20:37:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+dconst express = require('express');
+
+const app = express();
+
+app.use('/', (req, res) => {
+  res.send('hello, world 15!')
+});
+
+app.use('/hello', (req, res) => {
+  res.send('hello!')
+});
+
+app.listen(3333, () => {
+  console.log('listening at http://localhost:3333');
+  setInterval(() => console.log('ping'), 5000);
+  console.warn('this is a warning');
+  let bigMessage = '';
+  for(let i = 0; i < 100; i++) {
+    bigMessage += 'this is a big message '
+  }
+  console.log(bigMessage);
+});
